8c0fc33d1bcd25bc08ea33fbdc35e68c32f8535d
benoit brandt <benoit.07@hotmail.com>
2022-11-04 15:11:52 +0100

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning previous seed"
+Project.destroy_all
+ProfessionnalExperience.destroy_all
+Training.destroy_all
+Trip.destroy_all
+
+# Project creation
+puts "Project Creation"
+
+Project.create!(title: "Next stop",
+                description: "Have a good trip in public transport",
+                link: "www.next-stop.fr",
+                professional: true,
+                start_date: Date.new(2022, 9, 1),
+                end_date: Date.new(2022, 9, 9), user: User.last)
+
+# ProfessionalExperiences creation
+puts "ProfessionalExperiences Creation"
+
+ProfessionnalExperience.create!(title: "Automaticien",
+                                description: "Upgrading printing machine",
+                                location: "Mex",
+                                start_date: Date.new(2022, 9, 1),
+                                end_date: Date.new(2022, 9, 9),
+                                user: User.last)
+
+# Training creation
+puts "Training Creation"
+
+Training.create!(title: "Le wagon",
+                 description: "Learn to code",
+                 location: "Nantes",
+                 start_date: Date.new(2022, 9, 1),
+                 end_date: Date.new(2022, 9, 9),
+                 user: User.last)
+
+# Trip creation
+puts "Trip Creation"
+
+Trip.create!(to_work: false,
+             country: "france",
+             start_date: Date.new(2022, 7, 11),
+             end_date: Date.new(2022, 10, 21),
+             user: User.last)

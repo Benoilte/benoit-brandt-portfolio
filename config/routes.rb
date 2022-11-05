a7977cd6d55ccf6644d4c227754a5e3d7785e417
benoit brandt <benoit.07@hotmail.com>
2022-11-05 23:10:36 +0100

    add index method to professionnal experiences controller + create my-name partial

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,5 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :professional_experiences, only: [:index]
 end
---
+++ test/controllers/professional_experiences_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class ProfessionalExperiencesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end

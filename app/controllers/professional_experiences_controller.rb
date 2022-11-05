42e0f6b3f55ca80605e92fce3fe459f2f888617f
benoit brandt <benoit.07@hotmail.com>
2022-11-05 23:17:47 +0100

    add content to index page

--- app/controllers/professional_experiences_controller.rb
+++ app/controllers/professional_experiences_controller.rb
@@ -1,5 +1,7 @@
 class ProfessionalExperiencesController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
   def index
+    @professional_experiences = ProfessionalExperience.all
+    @trainings = Training.all
   end
 end

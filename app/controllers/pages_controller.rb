235e3de26ca76e933a43e9ef04920b23749176ce
benoit brandt <benoit.07@hotmail.com>
2022-11-18 10:04:02 +0100

    add routes - action - view and display render under construction for about section

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,10 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: %i[home about]
   def home
     @professionalexperiences = ProfessionalExperience.all
     @trainings = Training.all
+  end
+
+  def about
   end
 end

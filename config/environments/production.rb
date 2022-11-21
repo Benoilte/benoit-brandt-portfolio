aa3144ce811c92c97772aca1fd05dc7c529daa61
benoit brandt <benoit.07@hotmail.com>
2022-11-21 20:20:54 +0100

    configure MailCatcher - development env and prod. env to test mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,8 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "https://benoitbrandt.ch" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: "https://www.benoitbrandt.ch/" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.

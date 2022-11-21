5b254d19349a6e3cd8fdb7196fc0228c5bb7b4fc
benoit brandt <benoit.07@hotmail.com>
2022-11-21 22:41:44 +0100

    Updating production mailer settings to use MailGun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,12 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.action_mailer.delivery_method     = :postmark
-  config.action_mailer.postmark_settings   = { api_token: ENV.fetch('POSTMARK_API_TOKEN') }
+  config.delivery_method = :smtp
+
+  # use it with postkmark
+  # config.action_mailer.delivery_method     = :postmark
+  # config.action_mailer.postmark_settings   = { api_token: ENV.fetch('POSTMARK_API_TOKEN') }
+
   config.action_mailer.default_url_options = { host: "https://www.benoitbrandt.ch/" }
   # Settings specified here will take precedence over those in config/application.rb.
 

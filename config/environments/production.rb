9dd2434e329a9526082d310224cb26258379a9b9
benoit brandt <benoit.07@hotmail.com>
2022-11-22 12:25:59 +0100

    move config for mailgun only in production environnement

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,16 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  # config for MailGun adds on
+  config.smtp_settings = {
+    port: ENV.fetch('MAILGUN_SMTP_PORT'),
+    address: ENV.fetch('MAILGUN_SMTP_SERVER'),
+    user_name: ENV.fetch('MAILGUN_SMTP_LOGIN'),
+    password: ENV.fetch('MAILGUN_SMTP_PASSWORD'),
+    domain: 'benoitbrandt.ch/', # UPDATE THIS VALUE WITH YOUR OWN APP
+    authentication: :plain
+  }
+
   config.delivery_method = :smtp
 
   # use it with postkmark

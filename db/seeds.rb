584f5324840411e7da1eeb940b509fca5ae237d5
benoit brandt <benoit.07@hotmail.com>
2022-11-23 16:00:28 +0100

    correct seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -51,6 +51,7 @@ Training.create!(title: "CFC Automaticien",
                  location: "Mex",
                  start_date: Date.new(2022, 9, 1),
                  end_date: Date.new(2022, 9, 9),
+                 link: "",
                  user: User.last)
 
 Training.create!(title: "Technical maturity",
@@ -59,6 +60,7 @@ Training.create!(title: "Technical maturity",
                  location: "Yverdon-les-bains",
                  start_date: Date.new(2022, 9, 1),
                  end_date: Date.new(2022, 9, 9),
+                 link: "",
                  user: User.last)
 
 Training.create!(title: "Developer fullstack",
@@ -67,6 +69,7 @@ Training.create!(title: "Developer fullstack",
                  location: "Nantes",
                  start_date: Date.new(2022, 9, 1),
                  end_date: Date.new(2022, 9, 9),
+                 link: "",
                  user: User.last)
 
 # Trip creation

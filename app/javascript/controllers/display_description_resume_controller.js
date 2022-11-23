a455e2835c19dc5d19fd3e7c61954dd708397751
benoit brandt <benoit.07@hotmail.com>
2022-11-23 19:39:01 +0100

    separate data from work and study

--- app/javascript/controllers/display_description_resume_controller.js
+++ app/javascript/controllers/display_description_resume_controller.js
@@ -3,7 +3,8 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="display-description-resume"
 export default class extends Controller {
   connect() {
-    this.jsonData = [];
+    this.workData = [];
+    this.studyData = [];
     this.getJsonData()
   }
 
@@ -12,7 +13,12 @@ export default class extends Controller {
     fetch(this.url)
     .then(response =>response.json())
     .then(response =>response.forEach(data => {
-      this.jsonData.push(data);
+      console.log(typeof data.work_id === "undefined");
+      if(typeof data.work_id !== "undefined") {
+        this.workData.push(data);
+      } else {
+        this.studyData.push(data)
+      }
     }))
   }
 

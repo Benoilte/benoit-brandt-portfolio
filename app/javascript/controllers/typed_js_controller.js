3158a6edeb8560c1e917cc13c47d33a8bcf980c9
benoit brandt <benoit.07@hotmail.com>
2022-11-23 14:22:59 +0100

    set typed js to display on resume page

--- app/javascript/controllers/typed_js_controller.js
+++ app/javascript/controllers/typed_js_controller.js
@@ -1,7 +1,15 @@
 import { Controller } from "@hotwired/stimulus"
+import Typed from "typed.js"
 
 // Connects to data-controller="typed-js"
 export default class extends Controller {
+  static targets = ["explication"]
+
   connect() {
+    new Typed(this.explicationTarget, {
+      strings: ["click on one experience to see the description"],
+      typeSpeed: 50,
+      loop: false
+    })
   }
 }

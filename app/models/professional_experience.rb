e25d901fd615dad4035321af1e9821fcd2f6341c
benoit brandt <benoit.07@hotmail.com>
2022-11-30 01:21:02 +0100

    add method to get duration of working time

--- app/models/professional_experience.rb
+++ app/models/professional_experience.rb
@@ -1,3 +1,35 @@
 class ProfessionalExperience < ApplicationRecord
+  include ActionView::Helpers::TextHelper
+
   belongs_to :user
+
+  def duration_in_words
+    duration_array = duration_years_months
+    year_duration_in_word(duration_array[0]) + month_duration_in_word(duration_array[0], duration_array[1])
+  end
+
+  private
+
+  def duration_years_months
+    last_day = end_date || Date.today
+    diff_in_days = (last_day - start_date).to_i
+    years = (diff_in_days / 365.25)
+    years_floored = years.floor
+    months = (years - years_floored) * 12
+    [years_floored, months.round]
+  end
+
+  def year_duration_in_word(year)
+    year.positive? ? pluralize(year, 'year') : ""
+  end
+
+  def month_duration_in_word(year, month)
+    if year.positive? && month.positive?
+      " and #{pluralize(month, 'month')}"
+    elsif year.zero?
+      pluralize(month, 'month')
+    else
+      ""
+    end
+  end
 end
--- app/views/professional_experiences/index.json.jbuilder
+++ app/views/professional_experiences/index.json.jbuilder
@@ -1,6 +1,7 @@
 json.array! @professional_experiences do |professional_experience|
   json.work_id professional_experience.id
   json.work_company professional_experience.company
+  json.work_duration professional_experience.duration_in_words
   json.work_description professional_experience.description
   json.work_link professional_experience.link
 end
@@ -8,6 +9,7 @@ end
 json.array! @trainings do |training|
   json.study_id training.id
   json.study_institution training.institution
+  json.study_duration training.duration_in_words
   json.study_description training.description
   json.study_link training.link
 end

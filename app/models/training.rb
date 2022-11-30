e170d924e1a7738b55090003b575f2a519a587ff
benoit brandt <benoit.07@hotmail.com>
2022-11-30 01:31:34 +0100

    adapt json file with start_date and end_date

--- app/models/training.rb
+++ app/models/training.rb
@@ -3,6 +3,10 @@ class Training < ApplicationRecord
 
   belongs_to :user
 
+  def select_end_date
+    end_date || Date.today
+  end
+
   def duration_in_words
     duration_array = duration_years_months
     year_duration_in_word(duration_array[0]) + month_duration_in_word(duration_array[0], duration_array[1])
@@ -11,7 +15,7 @@ class Training < ApplicationRecord
   private
 
   def duration_years_months
-    last_day = end_date || Date.today
+    last_day = select_end_date
     diff_in_days = (last_day - start_date).to_i
     years = (diff_in_days / 365.25)
     years_floored = years.floor

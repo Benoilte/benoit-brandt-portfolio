e14099df9f8dd627b00b8f58d92facd863d5a951
benoit brandt <benoit.07@hotmail.com>
2022-11-30 02:07:16 +0100

    adapt view for company info and time info

--- app/javascript/controllers/display_description_resume_controller.js
+++ app/javascript/controllers/display_description_resume_controller.js
@@ -32,16 +32,22 @@ export default class extends Controller {
       this.workId = this.workData.find(element => element.work_id === this.id);
       this.description = this.workId.work_description;
       this.company = this.workId.work_company;
+      this.startDate = this.workId.work_start_date;
+      this.endDate = this.workId.work_end_date;
+      this.duration = this.workId.work_duration;
       this.descriptionTextTarget.innerText = this.description
-      this.descriptionCompanyTarget.innerText = this.company
-      this.descriptionDateTarget.innerText = "Date -- date"
+      this.descriptionCompanyTarget.innerHTML = `<h1>${this.company}</h1>`
+      this.descriptionDateTarget.innerHTML = `<p><strong>from ${this.startDate} to ${this.endDate}</strong> ${this.duration}</p>`
     } else {
       this.studyId = this.studyData.find(element => element.study_id === this.id);
       this.description = this.studyId.study_description;
       this.institution = this.studyId.study_institution;
+      this.startDate = this.studyId.study_start_date;
+      this.endDate = this.studyId.study_end_date;
+      this.duration = this.studyId.study_duration;
       this.descriptionTextTarget.innerText = this.description
-      this.descriptionCompanyTarget.innerText = this.institution
-      this.descriptionDateTarget.innerText = "Date -- date"
+      this.descriptionCompanyTarget.innerHTML = `<h1>${this.institution}</h1>`
+      this.descriptionDateTarget.innerHTML = `<p><strong>from ${this.startDate} to ${this.endDate}</strong> ${this.duration}</p>`
     }
   }
 

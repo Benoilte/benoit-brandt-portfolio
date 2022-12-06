0b4ff7ed0c26a5bc51c65ac944519c6f804728dc
benoit brandt <benoit.07@hotmail.com>
2022-12-06 22:20:18 +0100

    create json data and get it for project page

--- app/javascript/controllers/display_description_projects_controller.js
+++ app/javascript/controllers/display_description_projects_controller.js
@@ -3,6 +3,23 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="display-description-projects"
 export default class extends Controller {
   connect() {
-    console.log("helooooo sanity check");
+    this.professionalProjectsData = [];
+    this.personnalProjects = [];
+    this.getJsonData()
+    console.log(this.professionalProjectsData);
+    console.log(this.personnalProjects);
+  }
+
+  getJsonData() {
+    this.url = "/projects.json"
+    fetch(this.url)
+    .then(response =>response.json())
+    .then(response =>response.forEach(data => {
+      if(typeof data.professional_project_id !== "undefined") {
+        this.professionalProjectsData.push(data);
+      } else {
+        this.personnalProjects.push(data)
+      }
+    }))
   }
 }

cc82c10534dbfe81ec6b6e78cded26f956107ca6
benoit brandt <benoit.07@hotmail.com>
2022-12-06 15:19:11 +0100

    structure project page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,6 +2,7 @@ class ProjectsController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
 
   def index
-
+    @professional_projects = Project.where(professional: true)
+    @personnal_projects = Project.where(professional: false)
   end
 end

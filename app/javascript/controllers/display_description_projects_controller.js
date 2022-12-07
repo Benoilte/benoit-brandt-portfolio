e7ec8961ec9ae8205d278cfc91c0b57924bd1d7a
benoit brandt <benoit.07@hotmail.com>
2022-12-07 16:35:48 +0100

    display description when clicking on project

--- app/javascript/controllers/display_description_projects_controller.js
+++ app/javascript/controllers/display_description_projects_controller.js
@@ -6,7 +6,7 @@ export default class extends Controller {
 
   connect() {
     this.professionalProjectsData = [];
-    this.personnalProjects = [];
+    this.personnalProjectsData = [];
     this.getJsonData()
   }
 
@@ -18,7 +18,7 @@ export default class extends Controller {
       if(typeof data.professional_project_id !== "undefined") {
         this.professionalProjectsData.push(data);
       } else {
-        this.personnalProjects.push(data)
+        this.personnalProjectsData.push(data)
       }
     }))
   }
@@ -29,26 +29,27 @@ export default class extends Controller {
     event.currentTarget.classList.remove("underline-link")
     this.id = event.params.id
     this.section = event.params.section
-    if(this.section === "work"){
-      this.workId = this.workData.find(element => element.work_id === this.id);
-      this.description = this.workId.work_description;
-      this.company = this.workId.work_company;
-      this.startDate = this.workId.work_start_date;
-      this.endDate = this.workId.work_end_date;
-      this.duration = this.workId.work_duration;
+    if(this.section === "professional"){
+      this.professionalProject = this.professionalProjectsData.find(element => element.professional_project_id === this.id);
+      console.log(this.professionalProjectId);
+      this.description = this.professionalProject.professional_project_description;
+      this.company = this.professionalProject.professional_project_title;
+      this.startDate = this.professionalProject.professional_project_start_date;
+      // this.endDate = this.professionalProject.professional_project_end_date;
+      // this.duration = this.professionalProject.professional_project_duration;
       this.descriptionTextTarget.innerText = this.description
-      this.descriptionCompanyTarget.innerHTML = `<h1>${this.company}</h1>`
-      this.descriptionDateTarget.innerHTML = `<p><strong>from ${this.startDate} to ${this.endDate}</strong> ${this.duration}</p>`
+      // this.descriptionCompanyTarget.innerHTML = `<h1>${this.company}</h1>`
+      // this.descriptionDateTarget.innerHTML = `<p><strong>from ${this.startDate} to ${this.endDate}</strong> ${this.duration}</p>`
     } else {
-      this.studyId = this.studyData.find(element => element.study_id === this.id);
-      this.description = this.studyId.study_description;
-      this.institution = this.studyId.study_institution;
-      this.startDate = this.studyId.study_start_date;
-      this.endDate = this.studyId.study_end_date;
-      this.duration = this.studyId.study_duration;
+      this.personnalProject = this.personnalProjectsData.find(element => element.personnal_project_id === this.id);
+      this.description = this.personnalProject.personnal_project_description;
+      this.institution = this.personnalProject.personnal_project_title;
+      this.startDate = this.personnalProject.personnal_project_start_date;
+      // this.endDate = this.personnalProject.personnal_project_end_date;
+      // this.duration = this.personnalProject.personnal_project_duration;
       this.descriptionTextTarget.innerText = this.description
-      this.descriptionCompanyTarget.innerHTML = `<h1>${this.institution}</h1>`
-      this.descriptionDateTarget.innerHTML = `<p><strong>from ${this.startDate} to ${this.endDate}</strong> ${this.duration}</p>`
+      // this.descriptionCompanyTarget.innerHTML = `<h1>${this.institution}</h1>`
+      // this.projectDateTarget.innerHTML = `<p><strong>from ${this.startDate} to ${this.endDate}</strong> ${this.duration}</p>`
     }
   }
 

bd25951b0eb7205b0742dd6d23976ea8dc0219ec
benoit brandt <benoit.07@hotmail.com>
2022-12-08 20:08:03 +0100

    add routes + action + template show for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,8 +1,12 @@
 class ProjectsController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     @professional_projects = Project.where(professional: true)
     @personnal_projects = Project.where(professional: false)
+  end
+
+  def show
+
   end
 end

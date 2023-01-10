0070a131bc44b0654cd3584dc3d5ea8508afc4f8
benoit brandt <benoit.07@hotmail.com>
2023-01-10 19:48:43 +0100

    display days if time is less than 30 days

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ApplicationRecord
   include ActionView::Helpers::TextHelper
-  
+
   belongs_to :user
   has_one_attached :photo
 
@@ -10,7 +10,8 @@ class Project < ApplicationRecord
 
   def duration_in_words
     duration_array = duration_years_months
-    year_duration_in_word(duration_array[0]) + month_duration_in_word(duration_array[0], duration_array[1])
+    year_and_month_duration = year_duration_in_word(duration_array[0]) + month_duration_in_word(duration_array[0], duration_array[1], duration_array[2])
+    year_and_month_duration.present? ? year_and_month_duration : day_duration_in_word(duration_array[2])
   end
 
   private
@@ -21,20 +22,26 @@ class Project < ApplicationRecord
     years = (diff_in_days / 365.25)
     years_floored = years.floor
     months = (years - years_floored) * 12
-    [years_floored, months.round]
+    [years_floored, months.round, diff_in_days]
   end
 
   def year_duration_in_word(year)
     year.positive? ? pluralize(year, 'year') : ""
   end
 
-  def month_duration_in_word(year, month)
+  def month_duration_in_word(year, month, day)
     if year.positive? && month.positive?
       " and #{pluralize(month, 'month')}"
-    elsif year.zero?
+    elsif month.zero?
+      ""
+    elsif year.zero? && day >= 30
       pluralize(month, 'month')
     else
       ""
     end
+  end
+
+  def day_duration_in_word(day)
+    "#{pluralize(day, 'day')}"
   end
 end
